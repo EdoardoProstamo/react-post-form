a07d5dc582177da01a65e9cd5869b49822e0ced4
EdoardoProstamo <edoardoprostamo.19@libero.it>
2025-04-19 13:01:26 +0200

    feat: struttura form

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
 import { useState } from 'react'
-import './App.css'
 
 function App() {
 
   return (
-    <>
+    <div>
+      <h1>Campo testo</h1>
+      <form>
 
-    </>
+        <div>
+          <label>author</label>
+          <input type="text" />
+        </div>
+
+        <div>
+          <label>title</label>
+          <input type="text" />
+        </div>
+
+        <div>
+          <label>body</label>
+          <input type="text" />
+        </div>
+
+        <div>
+          <label>public</label>
+          <input type="boolean" />
+        </div>
+
+        <button>Invia</button>
+
+      </form>
+    </div>
+
   )
 }
 
